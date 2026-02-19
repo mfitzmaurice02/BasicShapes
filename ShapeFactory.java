918964c07721838da0969f581c75ca9be31100d0
Maurice Odhran Fitzmaurice <mfitzmaurice02@qub.ac.uk>
2026-02-19 10:23:38 +0000

    Add support for triangle shape in ShapeFactory

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -1,9 +1,2 @@
-public class ShapeFactory {
-    public Shape getShape(String id) {
-        switch(id.toLowerCase()) {
-            case "square": return new Square();
-            case "circle": return new Circle();
-            default: return null;
-        }
-    }
-}
+    case "triangle":
+        return new Triangle();
